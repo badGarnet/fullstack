3c987bc888cf6c0e0d7893eebb84726e8cb25572
Yao You <theyaoyou@gmail.com>
2018-06-24 20:04:06 -0500

    added MenuComponent to display menu items using Media list style"

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuConmponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar dark color='primay'>
-          <div className='container'>
+      <div>
+        <Navbar color="dark">
+          <div className="container">
             <NavbarBrand href="/">Restaurant Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
