9c93a6f94791e729969f76e54920dfecc4908f03
Yao You <theyaoyou@gmail.com>
2018-06-24 20:12:23 -0500

    added price for each item in the menu

--- React/confusion/src/components/MenuConmponent.js
+++ React/confusion/src/components/MenuConmponent.js
@@ -58,7 +58,7 @@ class Menu extends Component {
                             <Media object src={dish.image} alt={dish.name}  />
                         </Media>
                         <Media body className='ml-5'>
-                            <Media heading>{dish.name}</Media>
+                            <Media heading>{dish.name}, <i>${dish.price}</i></Media>
                             <p>{dish.description}</p>
                         </Media>
                     </Media>
